53dd224d46bcd6f6dbb535146aa9131f56f0ad8c
msydor <msydor@brella.com>
2021-04-02 18:39:30 -0500

    - Get rid of the var declaration;

--- src/utils.js
+++ src/utils.js
@@ -1,79 +1,69 @@
-import * as React from 'react'
+import * as React from "react";
 
 function useLocalStorageState(
   key,
-  defaultValue = '',
-  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+  defaultValue = "",
+  { serialize = JSON.stringify, deserialize = JSON.parse } = {}
 ) {
   const [state, setState] = React.useState(() => {
-    const valueInLocalStorage = window.localStorage.getItem(key)
+    const valueInLocalStorage = window.localStorage.getItem(key);
     if (valueInLocalStorage) {
-      return deserialize(valueInLocalStorage)
+      return deserialize(valueInLocalStorage);
     }
-    return typeof defaultValue === 'function' ? defaultValue() : defaultValue
-  })
+    return typeof defaultValue === "function" ? defaultValue() : defaultValue;
+  });
 
-  const prevKeyRef = React.useRef(key)
+  const prevKeyRef = React.useRef(key);
 
   React.useEffect(() => {
-    const prevKey = prevKeyRef.current
+    const prevKey = prevKeyRef.current;
     if (prevKey !== key) {
-      window.localStorage.removeItem(prevKey)
+      window.localStorage.removeItem(prevKey);
     }
-    prevKeyRef.current = key
-    window.localStorage.setItem(key, serialize(state))
-  }, [key, state, serialize])
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, state, serialize]);
 
-  return [state, setState]
+  return [state, setState];
 }
 
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
-
-
 const lightOrDark = (color) => {
-
   // Variables for red, green, blue values
-  var r, g, b, hsp;
+  let r;
+  let g;
+  let b;
+  let hsp;
 
   // Check the format of the color, HEX or RGB?
   if (color.match(/^rgb/)) {
-
     // If RGB --> store the red, green, blue values in separate variables
-    color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+    color = color.match(
+      /^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/
+    );
 
     r = color[1];
     g = color[2];
     b = color[3];
-  }
-  else {
-
+  } else {
     // If hex --> Convert it to RGB: http://gist.github.com/983661
-    color = +("0x" + color.slice(1).replace(
-      color.length < 5 && /./g, '$&$&'));
+    color = +("0x" + color.slice(1).replace(color.length < 5 && /./g, "$&$&"));
 
     r = color > 16;
-    g = color > 8 & 255;
+    g = (color > 8) & 255;
     b = color & 255;
   }
 
   // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
-  hsp = Math.sqrt(
-    0.299 * (r * r) +
-    0.587 * (g * g) +
-    0.114 * (b * b)
-  );
+  hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
 
   // Using the HSP value, determine whether the color is light or dark
-  if (hsp>127.5) {
-
-    return 'light';
+  if (hsp > 127.5) {
+    return "light";
+  } else {
+    return "dark";
   }
-  else {
+};
 
-    return 'dark';
-  }
-}
-
-
-export {useLocalStorageState, random, lightOrDark}
+export { useLocalStorageState, random, lightOrDark };
