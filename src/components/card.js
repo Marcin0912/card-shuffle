46b6449511efabab4bff8ef851f8ba9da76afea6
msydor <msydor@brella.com>
2021-04-07 20:53:37 -0500

    - Remove unnecessary function reassignment; - Get rid of else ctrl; - extract card styles to obj;

--- src/components/card.js
+++ src/components/card.js
@@ -1,46 +1,48 @@
 import React from "react";
-import Tilt from 'react-parallax-tilt';
+import Tilt from "react-parallax-tilt";
 
-const Card = React.forwardRef(({player, rotate, currentCard, index, children}, ref) => {
-  const rotateCard = rotate();
-  const top = currentCard() ? 0: rotateCard;
-  if(currentCard()) {
-    return (
+const Card = React.forwardRef(
+  ({ player, rotate, currentCard, index, children }, ref) => {
+    const top = currentCard() ? 0 : rotate();
+    const cardStyles = {
+      background: player.color,
+      color: player.textColor,
+      height: "17rem",
+      width: "30rem",
+      transformOrigin: "center",
+      transform: `rotate(${top}deg)`,
+    };
+
+    if (currentCard()) {
+      return (
         <div ref={ref} className=" relative Card">
-          <Tilt
-            scale={1.02}
-            perspective={1000}
-          >
-          <div style={{ background: player.color , color: player.textColor, height:"17rem", width: "30rem", transformOrigin: "center", transform: `rotate(${top}deg)`}}
-               className={`rounded-lg w-full m-auto flex relative px-12 ${currentCard() ? 'shadow-laura': ''}`}>
-              <span className="m-auto font-thin text-5xl">
-               {player.name}
-             </span>
-            {children}
-          </div>
+          <Tilt scale={1.02} perspective={1000}>
+            <div
+              style={cardStyles}
+              className={`rounded-lg w-full m-auto flex relative px-12 ${
+                currentCard() ? "shadow-laura" : ""
+              }`}
+            >
+              <span className="m-auto font-thin text-5xl">{player.name}</span>
+              {children}
+            </div>
           </Tilt>
         </div>
-    )
-  } else {
+      );
+    }
+
     return (
       <div ref={ref} className="color absolute Card">
-        <div style={{
-          background: player.color,
-          color: player.textColor,
-          height: "17rem",
-          width: "30rem",
-          transformOrigin: "center",
-          transform: `rotate(${top}deg)`
-        }} className="rounded-lg w-full m-auto flex relative px-12">
-        <span className="m-auto font-thin text-5xl">
-         {player.name}
-       </span>
+        <div
+          style={cardStyles}
+          className="rounded-lg w-full m-auto flex relative px-12"
+        >
+          <span className="m-auto font-thin text-5xl">{player.name}</span>
           {children}
         </div>
       </div>
-    )
+    );
   }
-});
-
+);
 
 export default Card;
