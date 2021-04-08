9503d13fd7d7f7ec90e908a86d7d41b84c8867cb
msydor <msydor@brella.com>
2021-04-07 20:41:29 -0500

    - Improve a11y on player input;

--- src/components/playerinputform.js
+++ src/components/playerinputform.js
@@ -1,15 +1,35 @@
 import React from "react";
 import Button from "./button";
 
-const PlayerInputForm = ({player, onAddPlayer}) => {
+const PlayerInputForm = ({ player, onAddPlayer }) => {
   return (
     <form onSubmit={onAddPlayer}>
-      <label htmlFor="player" className="uppercase text-white  py-2 text-left font-bold block w-full">Enter A Name</label>
-      <input id="player" style={{backgroundColor: "#616062"}} className="rounded py-5 px-10 pl-7
-       mb-2 w-full text-white" value={player} placeholder="First & Last" name="player"/>
-      <Button style={{backgroundColor: "#0b0a0c"}} className="uppercase font-bold  py-2.5 px-10 text-white rounded w-full shadow-laura" type="submit">SUBMIT (OR PRESS ENTER)</Button>
+      <label
+        htmlFor="player"
+        className="uppercase text-white  py-2 text-left font-bold block w-full"
+      >
+        Enter A Name *
+      </label>
+      <input
+        id="player"
+        style={{ backgroundColor: "#616062" }}
+        className="rounded py-5 px-10 pl-7
+       mb-2 w-full text-white"
+        value={player}
+        placeholder="First & Last"
+        name="player"
+        aria-required
+        required
+      />
+      <Button
+        style={{ backgroundColor: "#0b0a0c" }}
+        className="uppercase font-bold  py-2.5 px-10 text-white rounded w-full shadow-laura"
+        type="submit"
+      >
+        SUBMIT (OR PRESS ENTER)
+      </Button>
     </form>
-  )
-}
+  );
+};
 
 export default PlayerInputForm;
